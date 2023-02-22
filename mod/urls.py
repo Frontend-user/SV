df970fb9a350595da18c64cd673f588ce26afbeb
Robert <artemyoutubed@mail.ru>
2023-02-22 16:54:38 +0300

    urls

--- mod/urls.py
+++ mod/urls.py
@@ -1,8 +1,10 @@
-
 from django.contrib import admin
 from django.urls import path, include
 
+
+from . import views
+
 urlpatterns = [
-    path('mod/', include('mod.urls')),
+    path('', views.index, name='index'),
     path('admin/', admin.site.urls),
 ]
--- mod/views.py
+++ mod/views.py
@@ -2,14 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def index(request):
+def regist(request):
+
     return HttpResponse("Hello, world. You're at the polls index.")
 
-#
-# from django.urls import path
-#
-# from . import views
-#
-# urlpatterns = [
-#     path('', views.index, name='index'),
-# ]
